171e4250c8d5441e8752e12b90fd142985e0d39d
Julien Labatut <skoognuts@outlook.fr>
2022-02-07 19:05:59 +0100

    FIX: Fix Forestry Folders Tree.

--- .eleventy.js
+++ .eleventy.js
@@ -2,8 +2,13 @@
 
 module.exports = function(eleventyConfig) {
 
+  eleventyConfig.addCollection("prestations", function(collectionApi) {
+    return collectionApi.getFilteredByGlob("./src/content/prestations/*.md");
+  })
+
   eleventyConfig.addPassthroughCopy("./src/css");
   eleventyConfig.addPassthroughCopy("./src/assets");
+  eleventyConfig.addPassthroughCopy("./src/content");
 
   return {
     dir: {
