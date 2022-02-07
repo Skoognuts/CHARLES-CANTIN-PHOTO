f68ff9a9e1bec06c66c6f37c5c5b0a61dab648ed
Julien Labatut <skoognuts@outlook.fr>
2022-02-07 16:51:14 +0100

    INIT: Create React App.

---
+++ .eleventy.js
@@ -0,0 +1,14 @@
+/* Configuration File */
+
+module.exports = function(eleventyConfig) {
+
+  eleventyConfig.addPassthroughCopy("./src/css");
+  eleventyConfig.addPassthroughCopy("./src/assets");
+
+  return {
+    dir: {
+      input: "src",
+      output: "public"
+    }
+  }
+};
