5b7a0c33ebf789dafa24ad4b820521343db3a083
Julien Labatut <skoognuts@outlook.fr>
2022-02-08 18:57:28 +0100

    11TY: Config Gallery Script.

--- public/css/galerie.js
+++ public/css/galerie.js
@@ -1,9 +1,14 @@
 const categoryButtons = document.getElementsByClassName('categoryButton');
 const galerySlider = document.getElementById('galerySlider');
 const categoryButtonIds = [];
+
+if (!galerySlider.className) {
+  galerySlider.className = 'All';
+}
+
 for (let i = 0; i < categoryButtons.length; i++) {
   categoryButtonIds[i] = document.getElementById(categoryButtons[i].id);
   categoryButtonIds[i].addEventListener('click', () => {
-    galerySlider.innerText = categoryButtons[i].id;
+    galerySlider.className = categoryButtons[i].id;
   })
 }
